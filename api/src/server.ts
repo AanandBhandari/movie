51d81d898d9c73f43ca6c8e35f34afd90061c93d
Aanand <aanandbhandari143@gmail.com>
2022-03-10 09:17:04 +0545

    change static path

--- api/src/server.ts
+++ api/src/server.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 //db connection
 dbConnection();
 //server middleware
-app.use("/",express.static(__dirname + "/public"));
+// app.use("/",express.static(__dirname + "/public"));
 app.use(cors());
 app.use(express.json());
 app.use(
@@ -38,9 +38,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //404 error handler
-app.use("*", (req: Request, res: Response) =>
-  res.status(404).json(failure("Page not found."))
-);
+// app.use("*", (req: Request, res: Response) =>
+//   res.status(404).json(failure("Page not found."))
+// );
 
 // Global route error handling middleware
 app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
